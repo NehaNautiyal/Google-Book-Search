cb59ed92090bc4c470ed773787d501e14b4efc31
Neha Nautiyal <nehahaha@gmail.com>
2019-06-22 22:31:15 -0400

    functional app, api key undefined

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+let title = "flowers";
+let author = "keyes";
+
 const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=" + title + "+inauthor:" + author + "&key=";
-const APIKEY = require("../../../.env");
+const APIKEY = process.env.REACT_APP_GOOGLE_BOOKS_API;
+
+console.log(`${APIKEY}, apikey`);
 
 
 export default {
